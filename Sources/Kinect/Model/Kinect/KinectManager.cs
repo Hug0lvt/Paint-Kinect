ce3921c1c81864af21561793d7d1831a926d027a
Hugo LIVET <Hugo.LIVET@etu.uca.fr>
2024-01-09 17:04:28 +0100

    :sparkles: Manager good

--- Sources/Kinect/Model/Kinect/KinectManager.cs
+++ Sources/Kinect/Model/Kinect/KinectManager.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Kinect;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,20 +11,46 @@ namespace Model.Kinect
     public class KinectManager
     {
         public KinectSensor KinectSensor { get; set; }
-        public bool Status { get; private set; }
-        public string StatusText { get; private set; }
+        public bool Status { get; private set; } = false;
+        public string StatusText { get; private set; } = "Indisponible";
+
+        public KinectManager()
+        {
+            KinectSensor = KinectSensor.GetDefault();
+            KinectSensor.IsAvailableChanged += KinectSensor_IsAvailableChanged;
+        }
 
         public void StartSensor()
         {
-
+            if (KinectSensor != null && !KinectSensor.IsOpen)
+            {
+                KinectSensor.Open();
+            }
         }
 
         public void StopSensor()
         {
-
+            if (KinectSensor != null && KinectSensor.IsAvailable && KinectSensor.IsOpen)
+            {
+                KinectSensor.Close();
+            }
         }
 
-        //public void KinectSensor_IsAvailableChanged(sender, args)
+        public void KinectSensor_IsAvailableChanged(object sender, IsAvailableChangedEventArgs args)
+        {
+            if(KinectSensor != null && args.IsAvailable && KinectSensor.IsOpen)
+            {
+                Status = true;
+                StatusText = "Kinect Lancé !";
+                Debug.WriteLine("[LOG - KinectManager] (KinectSensor_IsAvailableChanged) : Kinect Start");
+            }
+            else
+            {
+                Status = false;
+                StatusText = "Kinect Eteint !";
+                Debug.WriteLine("[LOG - KinectManager] (KinectSensor_IsAvailableChanged) : Kinect Stop");
+            }
+        }
 
 
     }
